8373b07a6e0c2f0074439b6fc9f9e9e796750efa
Christabel <udodorchristabel@gmail.com>
2024-03-21 21:20:10 -0700

    modified

--- myPortfolio/models.py
+++ myPortfolio/models.py
@@ -6,3 +6,21 @@ class Message(models.Model):
     email = models.EmailField()
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.message} sent by {self.email}"
+
+class Education(models.Model):
+    education = models.CharField(max_length =100)
+    description = models.CharField(max_length =300)
+
+    pass
+
+
+class Skills(models.Model):
+    
+    pass
+
+
+class Porfolio(models.Model):
+    pass
