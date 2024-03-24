16040fc2bb9fc471628dbb017bc40daa1ddd2af5
Christabel <udodorchristabel@gmail.com>
2024-03-23 17:55:20 -0700

    modification for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4=p!213yjko#s%(_r=)(kli%p5_n+dg1g^k#o0)jr=$446^n)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
