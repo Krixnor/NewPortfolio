ac5bf2e9e8e54cf7d153505ad3963c6927c57b4b
Christabel <udodorchristabel@gmail.com>
2024-03-23 18:04:10 -0700

    modification for deployment

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('myPortfolio.urls')),
     path('admin/', admin.site.urls),
     path('myPortfolio/', include('myPortfolio.urls')),
 ]
